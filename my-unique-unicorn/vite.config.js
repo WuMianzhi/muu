51ff222f0c180328cb39c7e4a0bad0bc0f6fce60
sumangde@gmail.com <sumangde@gmail.com>
2025-05-12 21:44:36 +0800

    fix: 修复部署后国际化不生效的问题

--- my-unique-unicorn/vite.config.js
+++ my-unique-unicorn/vite.config.js
@@ -3,12 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import vueI18n from '@intlify/unplugin-vue-i18n/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    vueI18n({
+      include: path.resolve(__dirname, './src/locales/**')
+    })
   ],
   resolve: {
     alias: {
